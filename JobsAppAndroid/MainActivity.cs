4fb26595fbf5d4c5552099379a8df36bf4f1a9d8
Pearl Molefe <peamo@hotmail.com>
2020-02-18 16:13:45 +0200

    tablayout

---
+++ JobsAppAndroid/MainActivity.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V4.View;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using JobsAppAndroid.Fragments;
+
+namespace JobsAppAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private TabLayout tabLayout;
+        private ViewPager viewPager;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+
+            tabLayout = (TabLayout)FindViewById(Resource.Id.tablayout);
+            viewPager = (ViewPager)FindViewById(Resource.Id.viewpager);
+
+            SetupViewPager(viewPager);
+            tabLayout.SetupWithViewPager(viewPager);
+
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+        private AdapterFragment homeFragment;
+        private AdapterFragment jobsFragment;
+
+        public void SetupViewPager(ViewPager viewPager)
+        {
+            ViewPagerAdapter adapter = new ViewPagerAdapter(SupportFragmentManager);
+            homeFragment = new AdapterFragment();
+            jobsFragment = new AdapterFragment();
+
+        adapter.AddFragment(homeFragment, "Home");
+            adapter.AddFragment(jobsFragment, "Alerts");
+            viewPager.Adapter = adapter;
+        }
+
+    }
+}
+
---
+++ JobsAppAndroid/ViewPagerAdapter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using Android.Support.V4.App;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace JobsAppAndroid
+{
+    public class ViewPagerAdapter : FragmentPagerAdapter
+    {
+        private List<Android.Support.V4.App.Fragment> fragments = new List<Android.Support.V4.App.Fragment>();
+        private List<string> fragmentTitles = new List<string>();
+
+        public ViewPagerAdapter(Android.Support.V4.App.FragmentManager manager) : base(manager)
+        {
+            //base.OnCreate(manager);
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return fragments.Count;
+            }
+        }
+        public override Android.Support.V4.App.Fragment GetItem(int position)
+        {
+            return fragments[position];
+        }
+        public override ICharSequence GetPageTitleFormatted(int position)
+        {
+
+            return new Java.Lang.String(fragmentTitles[position].ToLower());// display the title
+            //return null;// display only the icon
+        }
+
+        public void AddFragment(Android.Support.V4.App.Fragment fragment, string title)
+        {
+            fragments.Add(fragment);
+            fragmentTitles.Add(title);
+        }
+
+    }
+}
