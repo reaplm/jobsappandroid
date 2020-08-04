7b9e8ceca3df31302dc82d05678b505f986482a5
PMolefe <PMolefe@G69BLAPICT80>
2020-08-04 20:12:35 +0200

    profile edit name/phone ui

--- JobsAppAndroid/ProfileActivity.cs
+++ JobsAppAndroid/ProfileActivity.cs
@@ -91,7 +91,29 @@ namespace JobsAppAndroid
         /// <param name="e"></param>
         private void EditPhone_Click(object sender, EventArgs e)
         {
+            LayoutInflater inflater = LayoutInflater.From(this);
+            View view = inflater.Inflate(Resource.Layout.dialog_profile_phone, null);
 
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.SetView(view);
+
+            var editPhone = view.FindViewById<EditText>(Resource.Id.dialog_edit_phone);
+            editPhone.Text = phone.Text;
+
+            alertBuilder.SetTitle("Edit Phone")
+                .SetPositiveButton("Submit", delegate
+                {
+                    Toast.MakeText(this, "You clicked Submit!", ToastLength.Short).Show();
+
+                })
+                .SetNegativeButton("Cancel", delegate
+                {
+                    alertBuilder.Dispose();
+
+                });
+
+            AlertDialog alertDialog = alertBuilder.Create();
+            alertDialog.Show();
 
         }
         /// <summary>
@@ -101,7 +123,55 @@ namespace JobsAppAndroid
         /// <param name="e"></param>
         private void EditName_Click(object sender, EventArgs e)
         {
-           
+            LayoutInflater inflater = LayoutInflater.From(this);
+            View view = inflater.Inflate(Resource.Layout.dialog_profile_name, null);
+
+            AlertDialog.Builder alertBuilder = new AlertDialog.Builder(this);
+            alertBuilder.SetView(view);
+
+            var fName = view.FindViewById<EditText>(Resource.Id.dialog_edit_fname);
+            var lName = view.FindViewById<EditText>(Resource.Id.dialog_edit_lname);
+
+            string[] fullName = name.Text.Split(" ");
+            fName.Text = fullName[0];
+            lName.Text = fullName[1];
+
+            alertBuilder.SetTitle("Edit Name")
+                .SetPositiveButton("Submit", delegate
+                {
+                    try
+                    {
+                        //get current user
+                        if (auth.CurrentUser != null)
+                        {
+                            var document = db.Collection("users").Document(auth.CurrentUser.Uid);
+                            var data = new Dictionary<string, Java.Lang.Object>();
+                            data.Add("FName", fName.Text);
+                            data.Add("LName", lName.Text);
+
+                            document.Update((IDictionary<string, Java.Lang.Object>)data);
+
+                            Toast.MakeText(this, "Done!", ToastLength.Long).Show();
+
+
+                        }
+                        else { Toast.MakeText(this, "Something went wrong. Sorry.", ToastLength.Long).Show(); }
+                    }
+                    catch (Exception ex)
+                    {
+                        Toast.MakeText(this, "Failed to update. Sorry.", ToastLength.Long).Show();
+                    }
+
+
+                })
+                .SetNegativeButton("Cancel", delegate
+                {
+                    alertBuilder.Dispose();
+
+                });
+
+            AlertDialog alertDialog = alertBuilder.Create();
+            alertDialog.Show();
         }
 
         public void OnSuccess(Java.Lang.Object result)
